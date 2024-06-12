412427b7687ca5d22cc38f9f783e5f84a41868e6
Ricardo Flores <40550552+refloresmaynasa@users.noreply.github.com>
2024-06-12 23:40:18 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,45 @@
-// create web server
-var express = require('express');
-var app = express();
+// Create web server
+// npm install express
+// npm install body-parser
 
-// create a route
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+app.use(bodyParser.json());
+
 app.get('/comments', function(req, res) {
-  res.send('This is the comments page');
+    fs.readFile('comments.json', 'utf8', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Server error');
+            return;
+        }
+        res.send(data);
+    });
 });
 
-// start the server
+app.post('/comments', function(req, res) {
+    fs.readFile('comments.json', 'utf8', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Server error');
+            return;
+        }
+        var comments = JSON.parse(data);
+        comments.push(req.body);
+        fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Server error');
+                return;
+            }
+            res.send('Success');
+        });
+    });
+});
+
 app.listen(3000, function() {
-  console.log('Server is running on http://localhost:3000');
+    console.log('Server is running');
 });
