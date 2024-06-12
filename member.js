75531182e59c2843e45f6b132a382877e466b6dc
Ricardo Flores <40550552+refloresmaynasa@users.noreply.github.com>
2024-06-12 23:33:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,37 @@
+function skillsMember() {
+    const skills = [
+        {
+            name: 'Javascript',
+            level: 90
+        },
+        {
+            name: 'React',
+            level: 85
+        },
+        {
+            name: 'Node.js',
+            level: 80
+        },
+        {
+            name: 'MongoDB',
+            level: 80
+        },
+        {
+            name: 'Python',
+            level: 70
+        },
+        {
+            name: 'Docker',
+            level: 70
+        },
+        {
+            name: 'Kubernetes',
+            level: 60
+        },
+        {
+            name: 'CI/CD',
+            level: 75
+        }
+    ];
+    return skills;
+}
